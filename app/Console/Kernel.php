d8579652d8803c0a162cc271c502dc58b619bc2c
thiagopetherson <thiagopetherson@outlook.com>
2023-02-19 12:08:01 -0300

    Adicionando Schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Location;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +17,15 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();       
+        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $data = [
+                'name' => 'Oxygen Resort Paramaribo',
+                'latitude' => '5.84695410560674',
+                'longitude' => '-55.186949423385705'
+            ];
+            Location::create($data);
+        })->cron('* * * * *');;    
     }
 
     /**
